5322782543d72c68cc1698cc9c5dcd9d243872fe
cariuk <hademopilie@gmail.com>
2022-05-09 19:50:00 +0800

    Add Event

--- Assets/Scenes/CheckObjectCollition.cs
+++ Assets/Scenes/CheckObjectCollition.cs
@@ -4,7 +4,23 @@ using UnityEngine;
 
 public class CheckObjectCollition : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other){
-         Debug.Log("Hello: " + other.name);
+     //Detect collisions between the GameObjects with Colliders attached
+     float x;
+     void OnTriggerEnter(Collider col)
+    {
+        x += 1;
+        Debug.Log(col.name +" : "+x);
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        x += 1;
+        Debug.Log(col.name +" : "+x);
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        x += 1;
+        Debug.Log(col.name +" : "+x);
     }
 }
